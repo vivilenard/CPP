0da7138e6129a6bdef9280f22e7b16348ee5af25
VIVI <vivienlenard00@gmail.com>
2023-10-02 16:09:36 +0200

    works for any type of array now

--- 07/ex01/Iter.hpp
+++ 07/ex01/Iter.hpp
@@ -3,11 +3,19 @@
 
 #include <iostream>
 
-template <typename func>
-void	iter(std::string & array, size_t length, func f)
+template <typename T>
+void	print_out(T obj)
 {
-	f(array, length);
+	std::cout << obj << std::endl;
 }
 
+template <typename T>
+void	iter(T *array, size_t length, void f (T))
+{
+	if (!array)
+		return ;
+	for (size_t i = 0; i < length; i++)
+			f(array[i]);
+}
 
 #endif
--- 07/ex01/main.cpp
+++ 07/ex01/main.cpp
@@ -1,17 +1,10 @@
 #include "Iter.hpp"
 
-void	convert(std::string & array, size_t length)
-{
-	for (size_t i = 0; i < length; i++)
-	{
-		array[i] = std::toupper(array[i]);
-	}
-}
-
 int main ()
 {
-	std::string str = "Hey du!"; 
-	iter(str, str.length(), convert);
-	std::cout << str << std::endl;
+	std::string str[] = {"Hi", "du"};
+	//char str1[]= "hallo";
+	//int str2[] = {0, 5, 3, 8};
+	::iter(str, 2, print_out);
 	return 0;
 }
